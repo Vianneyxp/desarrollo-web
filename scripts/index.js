1000d20d0791d118b84739cb10eac6279ac7d7ae
Vianney <erikavianneyxilot@gmail.com>
2022-12-18 22:12:31 -0600

    ain

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+let nombre = false
+let facebook = "Reny_X"
+let instagram = "Reny_X"
+let whatsapp = 2216483979
+const mail  = "erikavianneyxilot@gmail.com"
+console.log (nombre)
+console.log ( {facebook,instagram} );
+console.log (whatsapp)
+console.log (mail)
+
+let usualrio = prompt ("Hola como te llamas?")
+let edadDelUsuario = prompt ("cuantos años tienes?")
+alert("Hola " + usualrio + " bienvenido a Reny")
+
+alert(`Contactanos en nuetra pagina de facebook ${facebook} o via whatsapp ${whatsapp}`)
+let pantalones = 21
+let chamarras = 10
+let blusas = "21"
+pantalones+chamarra = conjunto
+pantalones==blusas
