4cc03c8da1bba84d088bd12c0f3356a2bd1d5889
Vianney <erikavianneyxilot@gmail.com>
2023-01-29 23:04:55 -0600

    ain

--- scripts/funciones.js
+++ scripts/funciones.js
@@ -20,43 +20,41 @@ console.log ("El precio de la prenda con descuento es de:", mitadPrecio);
 //ARRON FUNCTION
 const saludar = {} => 
     console.log ("hola" + nombre + "bienvenuido a Reny");
-saludar{"Erika"};
+saludar {"Erika"};
+
+const producto = [
+    {id: 1, nombre: "Blusa", precio: 100, img: "img.jpg", talla: "U"},
+    {id: 2, nombre: "Pantalon", precio: 300, img: "img.jpg", talla: "Ch, M, G"},
+    {id: 3, nombre: "Vestido niña", precio: 250, img: "img.jpg", talla: "Ch"},
+    {id: 4, nombre: "Mameluco", precio: 220, img: "img.jpg", talla: "Ch, M"},
+    {id: 5, nombre: "Sueter", precio: 350, img: "img.jpg", talla: "G, Ex"},
+    {id: 6, nombre: "Sudadera", precio: 280, img: "img.jpg", talla: "M, G, Ex"},
+];
+
+let precios = [1, 2, 3, 4, 5, 6];
+const suma = precios.reduce[(acumulador, el) => acumulador + el]
+console.log(suma/6);
+
+const precioProducto =producto.filter(elemento => elemento.precio > 200)
+console.log(precioProducto); 
 
 const carrito = [];
 const agregarAlCarrito = (producto, carrito) => {
     carrito.push(producto)
-    console.log("Se agregó con exito el producto")
-}
-const producto1 = {
-    nombre: "Blusa",
-    imagen: "../imagenes/blusa.jpg",
-    precio: 100,
-    descripcion: "variedad de blusas para dama unitalla" 
-}
-const producto2 = {
-    nombre: "Pantalon",
-    imagen: "../imagenes/jeans.jpg",
-    precio: 300,
-    descripcion: "stretch, amolda tu figura"
-}
-const producto3 = {
-    nombre: "Sueter ",
-    imagen: "img.jpg"
-    precio: 360,
-    descripcion: "Variedad ee colores, con forro doble afelpado" 
-}
-const producto4 = {
-    nombre: "Vestido",
-    imagen: "img.jpg",
-    precio: 250,
-    descripcion: "Vestido formal nogro" 
-}
-agregarAlCarrito (producto1,carrito)
-agregarAlCarrito (producto2,carrito)
-agregarAlCarrito (producto3,carrito)
-agregarAlCarrito (producto4,carrito)
+    console.log("Se agregó con exito el", producto);
+};
 
-console.log(carrito)
+producto.forEach(precioProducto => {
+    console.log(precioProducto.precio)
+});
+ 
+agregarAlCarrito (producto, carrito)
+//agregarAlCarrito (producto1,carrito)
+//agregarAlCarrito (producto2,carrito)
+//agregarAlCarrito (producto3,carrito)
+//agregarAlCarrito (producto4,carrito)
+
+//console.log(carrito)
 
 
 let suma = 0;
@@ -69,3 +67,24 @@ for (i=0; i < producto.length; i++) {
  for (let i=0; i < producto.length; i++) {
     console.log ("Total del carrito ", suma += producto[i].precio);
 }
+
+const ordenAlfabetico = producto.sort ((elemento1,elemento2)=> {
+    if (elemento1.nombre === elemento2.nombre) {
+        return 0
+    } else if (elemento1.nombre < elemento2.nombre) {
+    } else {
+        return 1
+    }
+});
+console.log ("Productos ordenados alfabeticamente", ordenAlfabetico)
+
+const mayorAmenoor = producto.sort ((elemento1,elemento2)=> {
+    if (elemento1.precio === elemento2.precio) {
+        return 0
+    } else if (elemento1.nombre > elemento2.nombre) {
+    } else {
+        return 1
+    }
+});
+
+
